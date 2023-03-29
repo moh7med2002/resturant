46092d7a18a0c728197a1471f045d0d22440fd73
moh7amed2002 <moha7medheshamabohani@gmail.com>
2023-03-29 22:36:45 +0300

    admin auth

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -10,10 +10,7 @@ module.exports.register = async(req,res,next)=>
         const {email , password} = req.body;
         const admin = await Admin.findOne({where:{email:email}});
         if (admin){
-            // const error = new Error ('E-mail already found');
-            // error.statusCode = 403;
-            // throw error;
-            res.status(403).json({message_en:'E-mail already used' , message_ar:"الايميل مستخدم"});
+            return res.status(403).json({message_en:'E-mail already used' , message_ar:"الايميل مستخدم"});
         }
         const hashPass = await bcrypt.hash(password,12);
         const newAdmin = await Admin.create({
@@ -37,17 +34,11 @@ exports.login = async(req,res,next)=>{
     try{
         const currentAdmin = await Admin.findOne({where:{email:email}});
         if(!currentAdmin){
-            // const error = new Error('E-mail not found');
-            // error.statusCode = 422;
-            // throw error;
-            res.status(403).json({message_en:'E-mail not found' , message_ar:"الايميل غير موجود"});
+            return res.status(403).json({message_en:'E-mail not found' , message_ar:"الايميل غير موجود"});
         }
         const isPasswordMatch = await bcrypt.compare(pass,currentAdmin.password);
         if(!isPasswordMatch){
-            // const error = new Error('Invalid password');
-            // error.statusCode=422;
-            // throw error;
-            res.status(403).json({message_en:'Invalid password' , message_ar:"كلمة المرور خاطئة"});
+            return res.status(403).json({message_en:'Invalid password' , message_ar:"كلمة المرور خاطئة"});
         };
         const {password,...other} = {...currentAdmin.toJSON()}
         const token = jwt.sign({
